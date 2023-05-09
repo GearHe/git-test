eea547340bb776d8a54b09bc80afa6140ee7f334
hd <1840503485@qq.com>
2023-05-09 21:10:41 +0800

    first commit

---
+++ src/main/java/com/hdjava/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.hdjava.git;
+
+/**
+ * @author HD
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
